66d6767d099590704447794ab17d003e868b2e92
Serg1853 <sergey24226@gmail.com>
2023-12-08 11:06:37 +0200

    1ts

--- src/1.ts
+++ src/1.ts
@@ -10,9 +10,9 @@ class Student {
   //   public grade: string;
 
   constructor(public name: string, public age: number, public grade: string) {
-    this.name = name;
-    this.age = age;
-    this.grade = grade;
+    this.name;
+    this.age;
+    this.grade;
   }
 }
 
